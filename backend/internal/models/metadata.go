74f1c9aabae582e95a799b4b6ea21097e5282761
MBMYA <mustafa.alaraj@hotmail.com>
2024-07-07 15:16:17 +0400

    update metadata data model

--- backend/internal/models/metadata.go
+++ backend/internal/models/metadata.go
@@ -1,9 +1,16 @@
 package models
 
+type description struct {
+	Name      string `json:"name"`
+	StartDate string `json:"startDate"`
+	EndDate   string `json:"endDate"`
+}
 type Metadata struct {
-	Username            string   `json:"username"`
-	Picture             string   `json:"picture"`
-	Projects            []string `json:"projects"`
-	Biography           string   `json:"biography"`
-	ProjectsDescription []string `json:"projectDescription"`
+	Username          string        `json:"username"`
+	Picture           string        `json:"picture"`
+	Projects          []description `json:"projects"`
+	Biography         string        `json:"biography"`
+	ProjectsSummary   []string      `json:"projectsSummary"`
+	Educations        []description `json:"educations"`
+	EducationsSummary []string      `json:"educationsSummary"`
 }
