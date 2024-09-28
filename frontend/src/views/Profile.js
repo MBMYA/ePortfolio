03fed03bd49836f646cde272a5b3ffe3a0e72857
MBMYA <mustafa.alaraj@hotmail.com>
2024-09-28 23:17:21 +0400

    Refactoring profile page

--- frontend/src/views/Profile.js
+++ frontend/src/views/Profile.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Searchbar from "../components/layouts/Searchbar.jsx";
-import Side_bar from "../components/layouts/Sidebar.jsx";
 import {
     Button,
     Card,
@@ -14,7 +12,6 @@ import {
   } from "reactstrap";
 
 import "../assets/css/profile.css";
-
 import { withRouter } from "../services/with-router.js";
 import { getData } from "../services/metadata.js";
 
@@ -48,63 +45,84 @@ function Profile(props) {
         console.log(username);
       }, []); // Empty dependency array to run only once on mount
     
-
-
-    
-    return (
-            <div style={{ display: "flex", flexDirection: "row", height: "100vh", width: "100vw", backgroundColor: "#dbdbdb"}}>
-                <div className="side-bar">
-                    <Side_bar />
+      return (
+        <div className="container">
+            <div className="row mt-3 justify-content-center">
+                
+                <div className="col-auto">
+                    <nav-bar className="nav-link">
+                        <ul className="nav_links">
+                            <li><a href="#">ePortfolio</a></li>
+                            <li><a href="#">My Profile</a></li>
+                            <li><a href="#">Manual</a></li>
+                            <li><a href="#">Contact us</a></li>
+                        </ul>
+                    </nav-bar>
                 </div>
-                <div style={{ display: "flex", flexDirection: "column", height: "100vh", flex: "1"}}>
-                    <div className="top-bar">
-                        <nav-bar className="nav-link">
-                            <ul className="nav_links">
-                                <li><a href="#">ePortfolio</a></li>
-                                <li><a href="#">My Profile</a></li>
-                                <li><a href="#">Manual</a></li>
-                                <li><a href="#">Contact us</a></li>
-                            </ul>
-                        </nav-bar>
-                        <div className="searchBar">
-                            <Searchbar />
-                        </div>
-                        <a className="cta"><button>Sign Out</button></a>
-                    </div>
-                    <div className="main-component">
-                        <div className="left-feed">
-                            <div className="side-content">
-                                <div className="profile-header">
-                                    <img src="" alt="" className="profile-picture" />
-                                </div>
-                                <section className="profile-info">
-                                    <h3>{username}</h3>
-                                    <p className="title">Fresh graduate</p>
-                                </section>
-                                <hr className="divider" />
-                                <section className="profile-info">
-                                    <h3>Mustafa Alaraj</h3>
-                                    <p className="title">Fresh graduate</p>
-                                </section>
-                                <hr className="divider" />
-                                <section className="profile-info">
-                                    <h3>Mustafa Alaraj</h3>
-                                    <p className="title">Fresh graduate</p>
-                                </section>                            
-                            </div>
-                            <div className="side-content"></div>
-                        </div>
-                        <div className="main-feed">
-                            <div className="side-content"></div>
-                            <div className="side-content"></div>
-                        </div>
-                        <div className="right-feed">
-                            <div className="side-content"></div>
-                        </div>
-                    </div>
+                <div className="col-2">
+                    <a className="cta"><button>Sign Out</button></a>
                 </div>
             </div>
-    );
+            
+        </div>
+        
+      );
+
+    
+    // return (
+    //         <div style={{ display: "flex", flexDirection: "row", height: "100vh", width: "100vw", backgroundColor: "#dbdbdb"}}>
+    //             <div className="side-bar">
+    //                 <Side_bar />
+    //             </div>
+    //             <div style={{ display: "flex", flexDirection: "column", height: "100vh", flex: "1"}}>
+                    // <div className="top-bar">
+                    //     <nav-bar className="nav-link">
+                    //         <ul className="nav_links">
+                    //             <li><a href="#">ePortfolio</a></li>
+                    //             <li><a href="#">My Profile</a></li>
+                    //             <li><a href="#">Manual</a></li>
+                    //             <li><a href="#">Contact us</a></li>
+                    //         </ul>
+                    //     </nav-bar>
+                    //     <div className="searchBar">
+                    //         <Searchbar />
+                    //     </div>
+                    //     <a className="cta"><button>Sign Out</button></a>
+                    // </div>
+    //                 <div className="main-component">
+    //                     <div className="left-feed">
+    //                         <div className="side-content">
+    //                             <div className="profile-header">
+    //                                 <img src="" alt="" className="profile-picture" />
+    //                             </div>
+    //                             <section className="profile-info">
+    //                                 <h3>{username}</h3>
+    //                                 <p className="title">Fresh graduate</p>
+    //                             </section>
+    //                             <hr className="divider" />
+    //                             <section className="profile-info">
+    //                                 <h3>Mustafa Alaraj</h3>
+    //                                 <p className="title">Fresh graduate</p>
+    //                             </section>
+    //                             <hr className="divider" />
+    //                             <section className="profile-info">
+    //                                 <h3>Mustafa Alaraj</h3>
+    //                                 <p className="title">Fresh graduate</p>
+    //                             </section>                            
+    //                         </div>
+    //                         <div className="side-content"></div>
+    //                     </div>
+    //                     <div className="main-feed">
+    //                         <div className="side-content"></div>
+    //                         <div className="side-content"></div>
+    //                     </div>
+    //                     <div className="right-feed">
+    //                         <div className="side-content"></div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    // );
 }
 
 export default withRouter(Profile);
